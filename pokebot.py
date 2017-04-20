f6a30b63ea0e791ec6e7964f63b6bed4f3963db7
Bram Hoven <bram.hoven@hotmail.com>
2017-04-20 15:30:26 +0200

    [FIX] Changed the way it works and made it faster

--- pokebot.py
+++ pokebot.py
@@ -1,81 +1,58 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import re
-import sys
 import getpass
 import threading
-import docker
-from random import randint
+import requests
+import time
+from requests import Session
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from robobrowser import RoboBrowser
 
-client = docker.from_env()
-containers = client.containers()
-seleniums = [c for c in containers if c["Image"] == "danielfrg/selenium"]
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-def start():
-	email = raw_input("Email: ")
-	password = getpass.getpass("Password: ") 
-	interval = raw_input("Seconds between checks: ")
-	print ""
-	print "Let's get ready to poke!"
-	print ""
+session = Session()
+session.proxies = {"https" : "https://188.166.45.135:3128"}
+browser = RoboBrowser(history=False, parser="lxml")
 
-	runBot(email, password, interval)
-	driver.quit()
+def login(mail, pwd):
+	browser.open("https://facebook.com/login")
+	loginform = browser.get_form(id="login_form")
+	loginform["email"] = mail
+	loginform["pass"] = pwd
+	browser.session.headers["Referer"] = "https://facebook.com/login"
+	browser.submit_form(loginform)
+	print "Logged in\n"
 
-def facebook_login(email, pwd):
-	driver.get("https://www.facebook.com/login")
-	email = driver.find_element_by_css_selector("#email").send_keys(email)
-	password = driver.find_element_by_css_selector("#pass").send_keys(pwd)
-	login = driver.find_element_by_css_selector("#loginbutton").submit()
-	return True
-
-def facebook_checkpokes():
-	pokes = driver.find_elements_by_xpath("//div[contains(@id, 'poke_live_item_')]")
+def check_pokes():
+	browser.open("https://facebook.com/pokes")
+	pokes = browser.find_all(id=re.compile('^poke_live_item_'))
 	return pokes
 
-def facebook_run(email, password, interval):
-	global driver
+def facebook_poke(pokes):
+	for poke in pokes:
+		name = poke.find(class_="_42us").find("a").getText() 
+		amount = poke.find(class_="uiHelpLink")["data-tooltip-content"]
+		url = poke.find(class_="selected")["ajaxify"]
+		browser.open("https://facebook.com/" + url)
 
+		print "----------------"
+		print name
+		print amount
+		print "Poked him back"
+		print "----------------\n"
 
-	selenium = seleniums[randint(0, len(seleniums)-1)]
-	try:
-		driver = webdriver.Remote(command_executor="http://127.0.0.1:"+str(selenium["Ports"][0]["PublicPort"])+"/wd/hub", desired_capabilities=DesiredCapabilities.CHROME)
-		facebook_login(email, password)
-		driver.get("https://www.facebook.com/pokes/")
-		if driver.find_element_by_xpath("//a[contains(@href,'/pokes/')]"):
-			pokes = facebook_checkpokes() 
-			infos = []
-
-			if pokes != None: 
-				for poke in pokes:
-					textdiv = poke.find_element_by_css_selector("._6a._42us")
-					name = textdiv.find_element(By.TAG_NAME, "a").text.encode("utf-8")
-					amount = textdiv.find_elements(By.TAG_NAME, "div")[1].text.encode("utf-8")
-					button = poke.find_element_by_css_selector(".selected")
-					link = button.get_attribute("ajaxify")
-					infos.append((name, amount, link))
-
-			for info in infos:
-				print "---------------"
-				print info[0], info[1]
-				print "Poked him back"
-				print "---------------"
-				print ""
-				driver.get("https://facebook.com" + info[2])
-	except:
-		print "Something went wrong"
-		print "Restarted container: " + selenium.id
-		selenium.restart()
-	finally:
-		driver.quit()
-		threading.Timer(interval, facebook_run(email, password, interval)).start()
-
-def runBot(email, password, interval):
-	facebook_run(email, password, interval)
+def facebook_run(mail, pwd, interval):
+	while True:
+		pokes = check_pokes()
+		if len(pokes) > 0:
+			facebook_poke(pokes)
+		time.sleep(float(interval))
+      
+    
 
 if __name__ == "__main__":
-    start()
+	mail = raw_input("Email: ")
+	pwd = getpass.getpass("Password: ")
+	interval = raw_input("Interval: ")
+	login(mail, pwd)
+
+	threading.Thread(interval, facebook_run(mail, pwd, interval)).start()
