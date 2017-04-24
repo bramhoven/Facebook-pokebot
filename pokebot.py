a9e3d971bec7866232ea5ca72b4cc6cb5a52db6e
bramhoven <bram.hoven@hotmail.com>
2017-04-24 22:19:00 +0200

    [ADD] Added an UI

--- pokebot.py
+++ pokebot.py
@@ -1,55 +1,64 @@
 import re
-import getpass
-import threading
 import requests
 import time
+import threading
 from requests import Session
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from robobrowser import RoboBrowser
 
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-browser = RoboBrowser(history=False, parser="lxml")
+class Pokebot(threading.Thread):
+	shutdown = False
+	stats = {}
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+	browser = RoboBrowser(history=False, parser="lxml")
 
-def login(mail, pwd):
-	browser.open("https://facebook.com/login")
-	loginform = browser.get_form(id="login_form")
-	loginform["email"] = mail
-	loginform["pass"] = pwd
-	browser.session.headers["Referer"] = "https://facebook.com/login"
-	browser.submit_form(loginform)
-	print "Logged in\n"
+	def __init__(self, mail, pwd, interval):
+		threading.Thread.__init__(self)
+		self.mail = mail
+		self.pwd = pwd
+		self.interval = interval
 
-def check_pokes():
-	browser.open("https://facebook.com/pokes")
-	pokes = browser.find_all(id=re.compile('^poke_live_item_'))
-	return pokes
+	def login(self, mail, pwd):
+		try:
+			self.browser.open("https://facebook.com/login")
+			loginform = self.browser.get_form(id="login_form")
+			if loginform != None:
+				loginform["email"] = mail
+				loginform["pass"] = pwd
+				self.browser.session.headers["Referer"] = "https://facebook.com/login"
+				self.browser.submit_form(loginform)
+		except:
+			return False
 
-def facebook_poke(pokes):
-	for poke in pokes:
-		name = poke.find(class_="_42us").find("a").getText() 
-		amount = poke.find(class_="_42us").find_all("div")[1].getText()
-		url = poke.find(class_="selected")["ajaxify"]
-		browser.open("https://facebook.com/" + url)
+	def check_pokes(self):
+		try:
+			self.browser.open("https://facebook.com/pokes")
+			pokes = self.browser.find_all(id=re.compile('^poke_live_item_'))
+			return pokes
+		except:
+			return []
 
-		print "----------------"
-		print name
-		print amount
-		print "Poked him back"
-		print "----------------\n"
+	def poke(self, pokes):
+		for poke in pokes:
+			name = poke.find(class_="_42us").find("a").getText() 
+			amount = poke.find(class_="_42us").find_all("div")[1].getText().replace(".", "")
+			url = poke.find(class_="selected")["ajaxify"]
+			try:
+				self.browser.open("https://facebook.com/" + url)
+				self.set_stat(name, int(re.search(r'\d+', amount).group()))
+			except:
+				return False
 
-def facebook_run(mail, pwd, interval):
-	while True:
-		pokes = check_pokes()
-		if len(pokes) > 0:
-			facebook_poke(pokes)
-		time.sleep(float(interval))
-      
-    
+	def set_stat(self, name, amount):
+		self.stats[name] = amount
+	
+	def get_stats(self):
+		return self.stats
 
-if __name__ == "__main__":
-	mail = raw_input("Email: ")
-	pwd = getpass.getpass("Password: ")
-	interval = raw_input("Interval: ")
-	login(mail, pwd)
-
-	threading.Thread(interval, facebook_run(mail, pwd, interval)).start()
+	def run(self):
+		self.login(self.mail, self.pwd)
+		while not self.shutdown:
+			pokes = self.check_pokes()
+			if len(pokes) > 0:
+				self.poke(pokes)
+			time.sleep(float(self.interval))	
---
+++ poker.py
@@ -0,0 +1,111 @@
+import curses
+import curses.panel
+import getpass
+import threading
+import time
+from pokebot import Pokebot
+
+shutdownStats = False
+shutdownRefresh = False
+
+def init():
+    global screen
+    global window_options
+    global window_stats
+    global mail
+    global pwd
+    global interval
+    global bot
+
+    mail = raw_input("Email: ")
+    pwd = getpass.getpass("Password: ")
+    interval = raw_input("Interval: ")
+    interval = "0"
+    bot = Pokebot(mail, pwd, interval)
+
+    screen = curses.initscr()
+    height, width = screen.getmaxyx()
+    screen.border(0)
+    curses.curs_set(0)
+
+    window_options = curses.newwin(9, width-2, 1, 1)
+    window_stats = curses.newwin(height-11, width-2, 10, 1)
+
+    reset_windows()
+    reset_windows()
+    threading.Thread(target=refresh).start()
+
+def reset_windows():
+    screen.border(0)
+    screen.refresh()
+
+    window_options.clear()
+    window_options.refresh()
+    window_options.border(0)
+    window_options.addstr(0, 1, "Facebook Pokebot")
+    window_options.addstr(2, 2, "[1] Start Bot")
+    window_options.addstr(3, 2, "[2] Stop Bot")
+    window_options.addstr(4, 2, "[3] Quit")
+    window_options.addstr(7, 2, "Select a option: ")
+    window_options.refresh()
+
+    window_stats.clear()
+    window_stats.refresh()
+    window_stats.border(0)
+    window_stats.addstr(0, 1, "Statistics")
+    window_stats.refresh()
+
+def refresh():
+    while not shutdownRefresh:
+        get_input()
+
+def refresh_stats():
+    while not shutdownStats:
+        stats = bot.get_stats()
+        index = 0
+        for name, amount in stats.iteritems():
+            window_stats.addstr(1+index, 1, name + ": " + str(amount) + " pokes")
+            index += 1
+
+        window_stats.refresh()
+    reset_windows()
+
+def get_input():
+    global bot
+    global shutdownStats
+
+    curses.echo()
+    option = screen.getstr(8, 20, 5)
+    curses.noecho()
+    if option == "1":
+        if not bot.isAlive():
+            bot.shutdown = False
+            shutdownStats = False
+            bot.start()
+            threading.Thread(target=refresh_stats).start()
+    elif option == "2":
+        if bot.isAlive():
+            bot.shutdown = True
+            shutdownStats = True
+            bot = Pokebot(mail, pwd, interval)
+    elif option == "3":
+        close()
+    elif option == "^C":
+        close()
+    else:
+        debug(" Wrong input ")
+    reset_windows()
+
+def debug(msg):
+    screen.addstr(24, 1, msg)
+
+def close():
+    if bot.isAlive():
+        bot.shutdown = True
+        shutdownStats = True
+        bot = Pokebot(mail, pwd, interval)
+    shutdownRefresh = True
+    curses.endwin()
+
+if __name__ == "__main__":
+    init()
